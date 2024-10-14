b51d77a11be85a3a24c64596794621aea8729504
Sam Lockart <sam.lockart@zendesk.com>
2024-10-14 13:49:10 +1100

    more config

--- flake.nix
+++ flake.nix
@@ -28,10 +28,14 @@
                   default = "/dev/stdout"; # 
                   description = "Path to the log file for pvpgn. Use stdout to print into the journal";
                 };
-                # "sql:mode=sqlite3;name=/var/pvpgn/users.db;default=0;prefix=pvpgn_"
                 database.type = lib.mkOption {
                   type = lib.types.str;
                   default = "sqlite3";
+                  description = "The type of database that bnetd should connect to.";
+                };
+                database.sqlitePath = lib.mkOption {
+                  type = lib.types.str;
+                  default = "/tmp/users.db";
                   description = "The type of database that bnetd should connect to.";
                 };
               };
@@ -55,7 +59,7 @@
               ExposedPorts = {"6112/tcp" = {};};
             };
           };
-          default = (with pkgs; with lib; stdenv.mkDerivation {
+          default = (with pkgs; with lib; with config; stdenv.mkDerivation {
             mainProgram = "bnetd";
             license = "gpl2";
             homepage = "https://github.com/pvpgn/pvpgn-server";
@@ -67,11 +71,9 @@
               development stopped in 2011, and aims to provide continued
               maintenance and additional features for PvPGN.
             '';
-            configureFlags = [ # add as configurable
-              "-D WITH_SQLITE3=true"
-              "-D WITH_LUA=true"
-              "-D LOCALSTATEDIR=/var/${name}"
-              "-D SYSCONFDIR=/etc/${name}"
+            cmakeFlags = [ # add as configurable
+              "-DWITH_SQLITE3=true"
+              "-DWITH_LUA=false" # requires lua5.1 https://stackoverflow.com/questions/10087226/lua-5-2-lua-globalsindex-alternative
             ];
             inherit version;
             pname = name;
@@ -80,13 +82,10 @@
               rev = version;
               sha256 = "sha256-6cY8Q2/fVs2swO1lvMK4vzDE4cU+JAtNhtXNrJwuS/w=";
             };
-            hardeningEnable = [ "pie" ];
             postInstall = ''
-              sed -i '\
-                s#^logfile.*#logfile = "/dev/stdout"#g;
-              ' $out/etc/pvpgn/bnetd.conf
-            '';
-            buildPhase = ''
+              sed -i 's#^logfile.*#logfile = "/dev/stdout"#g' $out/etc/pvpgn/bnetd.conf
+              sed -i 's#^storage_path.*#storage_path = "sql:mode=sqlite3;name=/tmp/users.db;default=0;prefix=pvpgn_"#g' $out/etc/pvpgn/bnetd.conf
+              sed -i 's#^ladderdir.*#ladderdir = "/tmp/ladders"#g' $out/etc/pvpgn/bnetd.conf
             '';
             nativeBuildInputs = [cmake gcc perl lua zlib sqlite];
           });
