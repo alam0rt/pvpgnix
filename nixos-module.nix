957f7d136d1e1444e32902f9efaf6fd98a5408c2
Sam Lockart <sam.lockart@zendesk.com>
2024-10-15 11:25:08 +1100

    try adding to module

--- nixos-module.nix
+++ nixos-module.nix
@@ -1,12 +1,37 @@
 { pkgs, config, lib, ... }:
-let cfg = config.services.pvpgn; in {
+let
+    name = "pvpgn";
+    cfg = config.services.pvpgn;
+    generators = lib.generators;
+
+    toConf = generators.toKeyValue {
+      mkKeyValue = generators.mkKeyValueDefault {} " = ";
+    };
+in {
     options = {
         services.pvpgn = {
             enable = lib.mkEnableOption "Enable the Battle.NET daemon";
 
             package = lib.mkOption {
-                description = "pvpgn package to use";
                 type = lib.types.package;
+                description = "pvpgn package to use";
+            };
+
+            user = lib.mkOption {
+                type = lib.types.str;
+                default = "pvpgn";
+                description = "user to run as";
+            };
+
+            group = lib.mkOption {
+                type = lib.types.str;
+                default = "pvpgn";
+                description = "group to run as";
+            };
+
+            localStateDir = lib.mkOption {
+                type = lib.types.str;
+                default = "/var/run/${name}";
             };
 
             logFile = lib.mkOption rec {
@@ -14,11 +39,14 @@ let cfg = config.services.pvpgn; in {
                 default = "/dev/stdout";
                 description = "Path to the log file for pvpgn. Use stdout to print into the journal";
             };
+
             openFirewall = lib.mkEnableOption "Allow PVPGN through the firewall.";
 
-            bnetd.configFile = lib.mkOption {
-                type = lib.types.str;
-                default = "${cfg.package}/etc/pvpgn/bnetd.conf";
+            bnetd = {
+                configFile = lib.mkOption {
+                  type = lib.types.str;
+                  default = "/etc/pvpgn/bnetd.conf";
+                };
             };
 
             database.type = lib.mkOption rec {
@@ -29,29 +57,39 @@ let cfg = config.services.pvpgn; in {
 
             database.sqlitePath = lib.mkOption rec {
                 type = lib.types.str;
-                default = "/tmp/users.db";
+                default = "${localusers.db";
                 description = "The type of database that bnetd should connect to.";
             };
         };
     };
 
     config = lib.mkIf config.services.pvpgn.enable {
+
+        environment.etc = {
+            bnetd: {
+                target = config.services.bnetd.configFile;
+                text = toConf {
+                    storage_path = "foo";
+                };
+            };
+        };
+
         systemd.services.bnetd = {
             after = ["network.target"];
             enable = true;
             serviceConfig = {
               ExecStart = "${cfg.package}/sbin/bnetd -f -c ${config.services.pvpgn.bnetd.configFile}";
-             # User = "pvpgn";
-             # Group = "pvpgn";
+              User = config.services.pvpgn.user;
+              Group = config.services.pvpgn.group;
             };
         };
-        users.users.pvpgn =
+        users.users.${config.services.pvpgn.user} =
           {
             isSystemUser = true;
-            group = "pvpgn";
+            group = config.services.pvpgn.group;
             description = "PVPGN user";
           };
-        users.groups.pvpgn = {};
+        users.groups.${config.services.pvpgn.group} = {};
 
         # firewall
         networking.firewall = lib.mkIf config.services.pvpgn.openFirewall {
